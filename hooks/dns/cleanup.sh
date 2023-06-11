fdc53a602b248579d71bcfd022ccc2a6c93bdc97
Axel Rindle <mail@axelrindle.de>
2023-06-11 13:05:44 +0200

    refactor: no content in cleanup hook

--- hooks/dns/cleanup.sh
+++ hooks/dns/cleanup.sh
@@ -5,8 +5,7 @@ source ./hooks/dns/common.sh
 RESPONSE=$(curl --silent \
     --request DELETE \
     --url "$LIMA_URL/domains/$LIMA_DOMAIN_ID/records/$CERTBOT_AUTH_OUTPUT" \
-    --user "$LIMA_USER:$LIMA_TOKEN" \
-    --header "Content-Type: application/json"
+    --user "$LIMA_USER:$LIMA_TOKEN"
 )
 
 echo $RESPONSE | jq .status
