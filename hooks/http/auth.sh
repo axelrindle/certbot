09daaf25bdd744874caddb07c93a661f54f51f88
Axel Rindle <mail@axelrindle.de>
2023-07-15 22:14:58 +0200

    fix: scp port

--- hooks/http/auth.sh
+++ hooks/http/auth.sh
@@ -4,5 +4,5 @@ source ./hooks/http/common.sh
 
 echo $CERTBOT_VALIDATION > ./$CERTBOT_TOKEN
 
-scp -i "$KEY" ./$CERTBOT_TOKEN "$USER@$HOST:$DEST"
+scp -i "$KEY" -P "$PORT" "./$CERTBOT_TOKEN" "$USER@$HOST:$DEST"
 ssh -i "$KEY" -p "$PORT" "$USER@$HOST" chmod 640 "$DEST/$CERTBOT_TOKEN"
